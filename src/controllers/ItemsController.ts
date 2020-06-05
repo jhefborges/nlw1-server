6f899d24a174bd2507b899a2cb945714229165e3
Jheferson Renan Borges <renanjheferson@gmail.com>
2020-06-05 17:32:17 -0300

    Parte 4 semana nlw

--- src/controllers/ItemsController.ts
+++ src/controllers/ItemsController.ts
@@ -10,7 +10,7 @@ class ItemControllers {
             return {
                 id: item.id,
                 title: item.title,
-                image_url: `http://localhost:3333/uploads/${item.image}`, 
+                image_url: `http://192.168.0.112:3333/uploads/${item.image}`, 
             };
         });
     
